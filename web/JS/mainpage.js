3d8183eb6db0bcd96784e39ce670ded698682103
unknown <wjiale960224@gmail.com>
2020-09-25 17:49:15 +0800

    mainpage layout and graphs

---
+++ web/JS/mainpage.js
@@ -0,0 +1,150 @@
+
+
+
+$(function (){
+    var double_pie = echarts.init(document.querySelector("#double_pie"));
+    var double_pie_option = {
+        tooltip: {
+            trigger: 'item',
+            formatter: '{a} <br/>{b}: {c} ({d}%)'
+        },
+        handleColors(){
+            let color = '';
+            let r=Math.floor(Math.random()*256);
+            let g=Math.floor(Math.random()*256);
+            let b=Math.floor(Math.random()*256);
+            color = `rgb(${r},${g},${b})`;
+            return color;
+        },
+
+        setEchartColor(){
+            for (let i of this.data.data.series) {
+                let color = this.actions.handleColors();
+                console.log('color',color)
+                console.log('i',i)
+                i['itemStyle']={color:color}
+            }
+        },
+        series: [
+            {
+                name: 'theme1',
+                type: 'pie',
+                selectedMode: 'single',
+                radius: [0, '50%'],
+
+                label: {
+                    position: 'inner'
+                },
+                labelLine: {
+                    show: false
+                },
+                data: [
+                    {value: 335, name: 'theme1', selected: true},
+                    {value: 679, name: 'theme2'},
+                    {value: 1548, name: 'theme3'},
+                    {value: 469, name: 'theme4'},
+                    {value: 518, name: 'theme5'},
+                ]
+            },
+            {
+                name: 'MSEQ_overview',
+                type: 'pie',
+                radius: ['60%', '75%'],
+                label: {
+                    show:false
+                },
+                labelLine: {
+                    show:false
+                },
+                data: [
+                    {value: 135, name: 'company1'},
+                    {value: 200, name: 'company2'},
+                    {value: 310, name: 'company3'},
+                    {value: 234, name: 'company4'},
+                    {value: 135, name: 'company5'},
+                    {value: 1048, name: 'company6'},
+                    {value: 251, name: 'company7'},
+                    {value: 147, name: 'company8'},
+                    {value: 102, name: 'company9'},
+                    {value: 469, name: 'company10'},
+                    {value: 518, name: 'company11'},
+                ]
+            }
+        ]
+    };
+    double_pie.setOption(double_pie_option);
+
+    var mseq_bar = echarts.init(document.querySelector("#mseq_bar"));
+    var mseq_bar_option = {
+        title: {
+            text: 'Investment and Raised'
+        },
+        color: ['rgb(173,139,46)'],
+        legend: {
+            data:['Value']
+        },
+        xAxis: {
+            data: ["MSEQ Invested","Total Capital Raised",]
+        },
+        yAxis: {},
+        series: [{
+            name: 'Value',
+            type: 'bar',
+            barWidth:40,
+            data: [5, 20,]
+        }]
+    };
+    mseq_bar.setOption(mseq_bar_option);
+
+    var two_pie = echarts.init(document.querySelector("#two_pie"));
+    var two_pie_option  = {
+        tooltip: {
+            trigger: 'item',
+            formatter: '{a} <br/>{b} : {c} ({d}%)'
+        },
+        series: [
+            {
+                name: 'Initial Date',
+                type: 'pie',
+                radius: '35%',
+                center: ['50%', '20%'],
+                data: [
+                    {value: 335, name: 'Series A'},
+                    {value: 310, name: 'Series B'},
+                    {value: 234, name: 'Series B1'},
+                    {value: 135, name: 'None'},
+                ],
+                emphasis: {
+                    itemStyle: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            },
+
+            {
+                name: 'Current Date',
+                type: 'pie',
+                radius: '35%',
+                center: ['50%', '80%'],
+                data: [
+                    {value: 135, name: 'Series A'},
+                    {value: 410, name: 'Series B'},
+                    {value: 334, name: 'Series B1'},
+                    {value: 35, name: 'None'},
+                ],
+                emphasis: {
+                    itemStyle: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }
+        ]
+    };
+
+    two_pie.setOption(two_pie_option);
+
+})
--- web/mainpage.jsp
+++ web/mainpage.jsp
@@ -12,41 +12,21 @@
     <meta http-equiv="Expires" content="0">
     <meta http-equiv="kiben" content="no-cache">
     <link rel="shortcut icon" href="#" />
-    <link rel="stylesheet" href="CSS/head_style.css" type="text/css">
+    <link rel="stylesheet" href="CSS/mainpage.css" type="text/css">
 </head>
 <body>
 <jsp:include page="header.jsp"></jsp:include>
-
 <section class = "mainbox">
-    <jsp:include page="left_guide.jsp"></jsp:include>
+    <div class = "column"><jsp:include page="left_guide.jsp"></jsp:include></div>
     <div class = "column">
-        <div class = "panel_first">
-            <div class = "company">
-                <span>Total Company:</span>&nbsp 31
-            </div>
-            <div class = "deals"><span>Total Deals:</span>&nbsp 6</div>
-            <div class = "invested"><span>Invested:</span>&nbsp 2.5M</div>
-            <div class = "l"><span>Left:</span>&nbsp 1.2M</div>
-        </div>
-        <div class = "panel_second">
-        </div>
+        <div class = "top_box"></div>
+        <div class = "bot_box">
+            <div class = "bot_column" id ="double_pie"></div>
+            <div class = "bot_column" id="two_pie"></div>
+            <div class = "bot_column" id ="mseq_bar"></div>
     </div>
-    <div class="column">
-        <div class = "panel_third">
-        </div>
-        <section class = "sub_box">
-            <div class = "column_2">
-                <div class="panel_pie1">
-                </div>
-            </div>
-            <div class = "column_2">
-                <div class="panel_pie2"></div>
-            </div>
-        </section>
-    </div>
-
 </section>
 <script src="${pageContext.request.contextPath}/JS/echarts-en.js"></script>
-<script src="${pageContext.request.contextPath}/JS/index.js"></script>
+<script src="${pageContext.request.contextPath}/JS/mainpage.js"></script>
 </body>
 </html>
