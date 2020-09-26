de0bd07202afc5a5165f3ea776c9784e2a24a90c
mightybeast <u6123487@anu.edu.au>
2020-09-26 12:43:12 +0800

    Udpate themes to match MSEQ defined themes.

--- src/com/xxxx/entity/Theme.java
+++ src/com/xxxx/entity/Theme.java
@@ -1,9 +1,9 @@
 package com.xxxx.entity;
 
 public enum Theme {
-    Healthcare,
-    Robotics,
-    Agtech,
-    Space,
-    AI,
+    Exponential_Machines,
+    Feeding_10B_People,
+    Humanity_Scale_Healthcare,
+    New_Society,
+    Space_Transport
 }
