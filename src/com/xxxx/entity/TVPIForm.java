5cd7d980f20b71a8ccd793bd64aa142290e8fad4
Xing <u6483085@anu.edu.au>
2020-10-17 11:38:44 +0800

    finish TVPIForm, need further test

--- src/com/xxxx/entity/TVPIForm.java
+++ src/com/xxxx/entity/TVPIForm.java
@@ -1,5 +1,7 @@
 package com.xxxx.entity;
 
+import java.sql.*;
+
 public class TVPIForm {
     String date;
     Integer TVPI;
@@ -7,7 +9,31 @@ public class TVPIForm {
     Double cumu_post_value;
     Double cumu_acquire_percent;
 
-    public void executeGetTVPI() {
+    public TVPIForm() { }
 
+    public TVPIForm(String date, Integer TVPI, Integer mseq_invest_to_date, Double cumu_post_value, Double cumu_acquire_percent) {
+        this.date = date;
+        this.TVPI = TVPI;
+        this.mseq_invest_to_date = mseq_invest_to_date;
+        this.cumu_post_value = cumu_post_value;
+        this.cumu_acquire_percent = cumu_acquire_percent;
+    }
+
+    public void executeGetTVPI() throws ClassNotFoundException, SQLException {
+        Connection conn = null;
+        CallableStatement call  = null;
+        ResultSet res = null;
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String url = "jdbc:mysql://msvc-database.ctmfp85b3w3e.ap-southeast-2.rds.amazonaws.com:3306/msvc?serverTimezone=UTC&useSSL=false&characterEcoding=UTF-8";
+        String username = "admin";
+        String password = "msvc2020";
+        conn = DriverManager.getConnection(url, username, password);
+
+        call = conn.prepareCall("{CALL getTVPI(?)}");
+        call.setString(1, date);
+
+        call.executeQuery();
+        System.out.println("Procedure Execute Ready.");
     }
 }
